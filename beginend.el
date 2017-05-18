116350375f162b3b928ae66808773706e8889dc9
Damien Cassou <damien@cassou.me>
2017-05-18 22:52:37 +0200

    Make linter happy

--- beginend.el
+++ beginend.el
@@ -27,7 +27,8 @@
 
 ;; Redefine M-< and M-> for some modes
 ;;
-;; - in dired mode, M-< (resp. M->) go to first (resp. last) file line
+;; - in dired mode, M-< (respectively M->) go to first (respectively last) file
+;;   line
 ;;
 ;; - in message mode,
 ;;    - M-< go to first line of message body (after headings)
