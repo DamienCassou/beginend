2fd6316a98871f2c5c40850fcd6f3e62127c8d17
Damien Cassou <damien@cassou.me>
2018-08-14 11:38:50 +0200

    Make Emacs 25.3 the minimum

--- test/beginend-marks-test.el
+++ test/beginend-marks-test.el
@@ -5,7 +5,7 @@
 ;; Author: Damien Cassou <damien@cassou.me>
 ;; Version: 2.0.0
 ;; URL: https://github.com/DamienCassou/beginend
-;; Package-requires: ((emacs "24.4"))
+;; Package-requires: ((emacs "25.3"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
